2ebb5390f64fa9535405d27f1d2a11d302fe89ff
Harrison Strowd <hstrowd@clairvista.com>
2015-02-02 08:36:29 -0600

    Adds support for moving back in the workflow.

--- app/controllers/new_patients_controller.rb
+++ app/controllers/new_patients_controller.rb
@@ -1,21 +1,56 @@
 class NewPatientsController < ApplicationController
   def submit_personal_details
-    redirect_to new_patient_tumor_path
+    if (params[:commit] == 'Cancel')
+      redirect_to root_path
+    elsif (params[:commit] == 'Back')
+      redirect_to root_path
+    else
+      # Default to 'Next'
+      redirect_to new_patient_tumor_path
+    end
   end
 
   def submit_tumor_details
-    redirect_to new_patient_work_path
+    if (params[:commit] == 'Cancel')
+      redirect_to root_path
+    elsif (params[:commit] == 'Back')
+      redirect_to new_patient_personal_path
+    else
+      # Default to 'Next'
+      redirect_to new_patient_work_path
+    end
   end
 
   def submit_work_details
-    redirect_to new_patient_life_path
+    if (params[:commit] == 'Cancel')
+      redirect_to root_path
+    elsif (params[:commit] == 'Back')
+      redirect_to new_patient_tumor_path
+    else
+      # Default to 'Next'
+      redirect_to new_patient_life_path
+    end
   end
 
   def submit_life_details
-    redirect_to new_patient_health_path
+    if (params[:commit] == 'Cancel')
+      redirect_to root_path
+    elsif (params[:commit] == 'Back')
+      redirect_to new_patient_work_path
+    else
+      # Default to 'Next'
+      redirect_to new_patient_health_path
+    end
   end
 
   def submit_health_details
-    redirect_to thank_you_path
+    if (params[:commit] == 'Cancel')
+      redirect_to root_path
+    elsif (params[:commit] == 'Back')
+      redirect_to new_patient_life_path
+    else
+      # Default to 'Next'
+      redirect_to thank_you_path
+    end
   end
 end
--- app/controllers/returning_patients_controller.rb
+++ app/controllers/returning_patients_controller.rb
@@ -1,9 +1,23 @@
 class ReturningPatientsController < ApplicationController
   def submit_personal_details
-    redirect_to returning_patient_health_path
+    if (params[:commit] == 'Cancel')
+      redirect_to root_path
+    elsif (params[:commit] == 'Back')
+      redirect_to root_path
+    else
+      # Default to 'Next'
+      redirect_to returning_patient_health_path
+    end
   end
 
   def submit_health_details
-    redirect_to thank_you_path
+    if (params[:commit] == 'Cancel')
+      redirect_to root_path
+    elsif (params[:commit] == 'Back')
+      redirect_to returning_patient_personal_path
+    else
+      # Default to 'Next'
+      redirect_to thank_you_path
+    end
   end
 end
