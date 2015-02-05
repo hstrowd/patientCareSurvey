f1a9fd7873966ecd824427863b5d6d6c88cd68a4
Harrison Strowd <hstrowd@clairvista.com>
2015-02-04 21:03:47 -0600

    Adds synchronization and JS controls to all necessary fields.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -30,10 +30,10 @@ window.patientCare = {
     },
     submitForm: function(formID, action, opts) {
         var $form = $('#' + formID);
-        if (!$form) { return; }
+        if ($form.legnth == 0) { return; }
 
         var $actionField = $form.find('input.action');
-        if ($actionField) {
+        if ($actionField.legnth != 0) {
             $actionField.val(action);
         }
 
@@ -66,5 +66,69 @@ window.patientCare = {
         if (isValid) {
             $form.submit();
         }
+    },
+    enabledControlField: function(formID, controlSelector, fieldSelector) {
+        var $form = $('#' + formID);
+        if ($form.legnth) { return; }
+
+        var $control = $form.find(controlSelector);
+        var $field = $form.find(fieldSelector);
+        if ($control.legnth || $field.legnth) {
+            return;
+        }
+
+        $control.change(function() {
+            if ($control.is(':checked')) {
+                $field.attr('disabled', 'disabled');
+                $field.val(null);
+            } else {
+                $field.removeAttr('disabled');
+            }
+        });
+    },
+    // Note: The source is expected to be updated by the user and the base
+    //   will be updated to reflect this.
+    reflectField: function(formID, baseSelector, sourceSelector) {
+        var $form = $('#' + formID);
+        if ($form.legnth) { return; }
+
+        var $base = $form.find(baseSelector);
+        var $source = $form.find(sourceSelector);
+        if ($base.legnth || $source.legnth) {
+            return;
+        }
+
+        $base.change(function() {
+            $source.val($base.val());
+        });
+        $source.change(function() {
+            $base.val($source.val());
+            $base.trigger('change');
+        });
+    },
+    // Note: The radio is expected to be updated by the user and the base field
+    //   will be updated to reflect this.
+    reflectRadio: function(formID, baseFieldSelector, radioName) {
+        var $form = $('#' + formID);
+        if ($form.legnth) {
+            return;
+        }
+
+        var $base = $form.find(baseFieldSelector);
+        var $radio = $form.find('input:radio[name=' + radioName + ']');
+        if ($base.legnth || $radio.legnth) {
+            return;
+        }
+
+        $base.change(function() {
+            var $checkedOption = $radio.filter('#' + radioName + '_' + $base.val());
+            if ($checkedOption.length) { return; }
+
+            $checkedOption.attr('checked', true);
+        });
+        $radio.change(function() {
+            $base.val($radio.filter(':checked').val());
+            $base.trigger('change');
+        });
     }
 }
